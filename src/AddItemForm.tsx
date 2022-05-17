e7eb6ee883eafe05fa44cba24116eaa0ec8f0075
Ilya Pasternak <pasterzoom@gmail.com>
2022-05-17 14:51:12 +0300

    17.05.22

---
+++ src/AddItemForm.tsx
@@ -0,0 +1,49 @@
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+import {IconButton, TextField} from '@material-ui/core';
+
+type AddItemFormPropsType = {
+    addItem: (title: string) => void
+}
+
+const AddItemForm = (props: AddItemFormPropsType) => {
+    const [title, setTitle] = useState<string>('')
+    const [error, setError] = useState<boolean>(false)
+    const onChangeSetTitle = (e: ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.currentTarget.value)
+        if (error) setError(false)
+    }
+    const onClickAddItem = () => {
+        const trimmedTitle = title.trim()
+        if (trimmedTitle) {
+            props.addItem(trimmedTitle)
+        } else {
+            setError(true)
+        }
+        setTitle('')
+    }
+    const onKeyPressAddItem = (e: KeyboardEvent<HTMLInputElement>) => {
+        e.key === 'Enter' && onClickAddItem()
+    }
+    return (
+        <div>
+            <TextField value={title}
+                       onChange={onChangeSetTitle}
+                       onKeyPress={onKeyPressAddItem}
+                       variant="outlined"
+                       error={error}
+                       id="outlined-error-helper-text"
+                       label={error ? 'Error' :'Enter text'}
+                       defaultValue="Hello World"
+                       helperText={error && 'Title is required!'}
+                       size={'small'}
+            />
+            <IconButton color={'primary'}
+                        onClick={onClickAddItem}>
+                <AddCircleOutlineIcon/>
+            </IconButton>
+        </div>
+    );
+};
+
+export default AddItemForm;
